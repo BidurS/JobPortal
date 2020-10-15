3ac92ced1d9f0b6f313511cbe45c1ad65dedd855
bidurs <bidurandblog@gmail.com>
2020-10-14 22:45:58 -0700

    final touch

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,8 +3,8 @@ import {Col, Form} from 'react-bootstrap'
 
 export default function SearchForm({ params, onParamChange}) {
     return (
-        <Form className = 'mb-4'>
-            <Form.Row>
+        <Form className='mb-2'>
+            <Form.Row className='align-items-end'>
                 <Form.Group as={Col}>
                     <Form.Label>Description</Form.Label>
                     <Form.Control onChange={onParamChange} value={params.description} name='description' type='text'/>
@@ -12,6 +12,9 @@ export default function SearchForm({ params, onParamChange}) {
                 <Form.Group as={Col}>
                     <Form.Label>Location</Form.Label>
                     <Form.Control onChange={onParamChange} value={params.location} name='location' type='text'/>
+                </Form.Group>
+                <Form.Group as={Col} XS='auto' className ='ml-2'>
+                    <Form.Check onChange={onParamChange} value={params.full_time} name='full_time' id='full-time' label='only Full Time' type='checkbox' className='mb-2'/>
                 </Form.Group>
             </Form.Row>
         </Form>
